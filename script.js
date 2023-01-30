941363fa0ef550a91811b79beb718cce8d9c2a5c
SeferX <seferibrahim2@gmail.com>
2023-01-30 14:37:42 +0300

    Update script for phrases

--- script.js
+++ script.js
@@ -1,95 +1,142 @@
-const musicContainer = document.getElementById('music-container');
-const playBtn = document.getElementById('play');
-const prevBtn = document.getElementById('prev');
-const nextBtn = document.getElementById('next');
+const musicContainer = document.getElementById("music-container");
+const playBtn = document.getElementById("play");
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
 
-const audio = document.getElementById('audio');
-const progress = document.getElementById('progress');
-const progressContainer = document.getElementById('progress-container');
-const title = document.getElementById('title');
-const cover = document.getElementById('cover');
+const audio = document.getElementById("audio");
+const progress = document.getElementById("progress");
+const progressContainer = document.getElementById("progress-container");
+const title = document.getElementById("title");
+const cover = document.getElementById("cover");
 
-const songs = ['hey', 'summer', 'ukulele'];
+const songs = [
+	"1",
+	"2",
+	"3",
+	"4",
+	"5",
+	"6",
+	"7",
+	"8",
+	"9",
+	"10",
+	"11",
+	"12",
+	"13",
+	"14",
+	"15",
+	"16",
+	"17",
+	"18",
+	"19",
+	"20",
+	"21",
+	"22",
+	"23",
+	"24",
+	"25",
+];
 
-let songIndex = 2;
+let timeout;
+
+let songIndex = 0;
 
 loadSong(songs[songIndex]);
 
 function loadSong(song) {
-  title.innerText = song;
-  audio.src = `music/${song}.mp3`;
-  cover.src = `images/${song}.jpg`;
+	title.innerText = song;
+	// audio.src = `music/${song}.mp3`;
+	audio.src = `mp3/${song}.mp3`;
+	// cover.src = `images/${song}.jpg`;
+	cover.src = `images/hey.jpg`;
 }
 
 function playSong() {
-  musicContainer.classList.add('play');
-  playBtn.querySelector('i.fas').classList.remove('fa-play');
-  playBtn.querySelector('i.fas').classList.add('fa-pause');
+	musicContainer.classList.add("play");
+	playBtn.querySelector("i.fas").classList.remove("fa-play");
+	playBtn.querySelector("i.fas").classList.add("fa-pause");
 
-  audio.play();
+	audio.play();
 }
 
 function pauseSong() {
-  musicContainer.classList.remove('play');
-  playBtn.querySelector('i.fas').classList.add('fa-play');
-  playBtn.querySelector('i.fas').classList.remove('fa-pause');
+	musicContainer.classList.remove("play");
+	playBtn.querySelector("i.fas").classList.add("fa-play");
+	playBtn.querySelector("i.fas").classList.remove("fa-pause");
+	clearInterval(timeout);
 
-  audio.pause();
+	audio.pause();
 }
 
 function prevSong() {
-  songIndex--;
+	clearInterval(timeout);
+	songIndex--;
 
-  if (songIndex < 0) {
-    songIndex = songs.length - 1;
-  }
+	if (songIndex < 0) {
+		songIndex = songs.length - 1;
+	}
 
-  loadSong(songs[songIndex]);
+	loadSong(songs[songIndex]);
 
-  playSong();
+	playSong();
 }
 
 function nextSong() {
-  songIndex++;
+	clearInterval(timeout);
+	songIndex++;
 
-  if (songIndex > songs.length - 1) {
-    songIndex = 0;
-  }
+	if (songIndex > songs.length - 1) {
+		songIndex = 0;
+	}
 
-  loadSong(songs[songIndex]);
+	loadSong(songs[songIndex]);
 
-  playSong();
+	playSong();
+}
+
+function continueToNextSong() {
+	timeout = setTimeout(() => {
+		songIndex++;
+
+		if (songIndex > songs.length - 1) {
+			songIndex = 0;
+		}
+
+		loadSong(songs[songIndex]);
+
+		playSong();
+	}, 5000);
 }
 
 function updateProgress(e) {
-  const { duration, currentTime } = e.srcElement;
-  const progressPercent = (currentTime / duration) * 100;
-  progress.style.width = `${progressPercent}%`;
+	const { duration, currentTime } = e.srcElement;
+	const progressPercent = (currentTime / duration) * 100;
+	progress.style.width = `${progressPercent}%`;
 }
 
 function setProgress(e) {
-  const width = this.clientWidth;
-  const clickX = e.offsetX;
-  const duration = audio.duration;
+	const width = this.clientWidth;
+	const clickX = e.offsetX;
+	const duration = audio.duration;
 
-  audio.currentTime = (clickX / width) * duration;
+	audio.currentTime = (clickX / width) * duration;
 }
 
-playBtn.addEventListener('click', () => {
-  const isPlaying = musicContainer.classList.contains('play');
+playBtn.addEventListener("click", () => {
+	const isPlaying = musicContainer.classList.contains("play");
 
-  if (isPlaying) {
-    pauseSong();
-  } else {
-    playSong();
-  }
+	if (isPlaying) {
+		pauseSong();
+	} else {
+		playSong();
+	}
 });
 
-prevBtn.addEventListener('click', prevSong);
-nextBtn.addEventListener('click', nextSong);
+prevBtn.addEventListener("click", prevSong);
+nextBtn.addEventListener("click", nextSong);
 
-audio.addEventListener('timeupdate', updateProgress);
+audio.addEventListener("timeupdate", updateProgress);
 
-progressContainer.addEventListener('click', setProgress);
+progressContainer.addEventListener("click", setProgress);
 
-audio.addEventListener('ended', nextSong);
+audio.addEventListener("ended", continueToNextSong);
