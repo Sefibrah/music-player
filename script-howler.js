29fbc28aa6116b9f5cef3bd56d726f8e34351faf
SeferX <seferibrahim2@gmail.com>
2023-02-08 15:57:15 +0300

    Fix howler.js attempt no. one million billion

--- script-howler.js
+++ script-howler.js
@@ -2,31 +2,31 @@ var context = new (window.AudioContext || window.webkitAudioContext)();
 context.suspend();
 
 var sounds = [
-	new Howl({ src: "mp3/1.mp3" }),
-	new Howl({ src: "mp3/2.mp3" }),
-	new Howl({ src: "mp3/3.mp3" }),
-	new Howl({ src: "mp3/4.mp3" }),
-	new Howl({ src: "mp3/5.mp3" }),
-	new Howl({ src: "mp3/6.mp3" }),
-	new Howl({ src: "mp3/7.mp3" }),
-	new Howl({ src: "mp3/8.mp3" }),
-	new Howl({ src: "mp3/9.mp3" }),
-	new Howl({ src: "mp3/10.mp3" }),
-	new Howl({ src: "mp3/11.mp3" }),
-	new Howl({ src: "mp3/12.mp3" }),
-	new Howl({ src: "mp3/13.mp3" }),
+	new Howl({ src: "mp3/1.mp3", html5: true }),
+	new Howl({ src: "mp3/2.mp3", html5: true }),
+	new Howl({ src: "mp3/3.mp3", html5: true }),
+	new Howl({ src: "mp3/4.mp3", html5: true }),
+	new Howl({ src: "mp3/5.mp3", html5: true }),
+	new Howl({ src: "mp3/6.mp3", html5: true }),
+	new Howl({ src: "mp3/7.mp3", html5: true }),
+	new Howl({ src: "mp3/8.mp3", html5: true }),
+	new Howl({ src: "mp3/9.mp3", html5: true }),
+	new Howl({ src: "mp3/10.mp3", html5: true }),
+	new Howl({ src: "mp3/11.mp3", html5: true }),
+	new Howl({ src: "mp3/12.mp3", html5: true }),
+	new Howl({ src: "mp3/13.mp3", html5: true }),
 	// { src: "mp3/14.mp3" },
-	new Howl({ src: "mp3/15.mp3" }),
-	new Howl({ src: "mp3/16.mp3" }),
-	new Howl({ src: "mp3/17.mp3" }),
-	new Howl({ src: "mp3/18.mp3" }),
-	new Howl({ src: "mp3/19.mp3" }),
-	new Howl({ src: "mp3/20.mp3" }),
-	new Howl({ src: "mp3/21.mp3" }),
-	new Howl({ src: "mp3/22.mp3" }),
-	new Howl({ src: "mp3/23.mp3" }),
-	new Howl({ src: "mp3/24.mp3" }),
-	new Howl({ src: "mp3/25.mp3" }),
+	new Howl({ src: "mp3/15.mp3", html5: true }),
+	new Howl({ src: "mp3/16.mp3", html5: true }),
+	new Howl({ src: "mp3/17.mp3", html5: true }),
+	new Howl({ src: "mp3/18.mp3", html5: true }),
+	new Howl({ src: "mp3/19.mp3", html5: true }),
+	new Howl({ src: "mp3/20.mp3", html5: true }),
+	new Howl({ src: "mp3/21.mp3", html5: true }),
+	new Howl({ src: "mp3/22.mp3", html5: true }),
+	new Howl({ src: "mp3/23.mp3", html5: true }),
+	new Howl({ src: "mp3/24.mp3", html5: true }),
+	new Howl({ src: "mp3/25.mp3", html5: true }),
 ];
 
 const musicContainer = document.getElementById("music-container");
@@ -46,7 +46,7 @@ function pause() {
 	sounds[currentSound].pause();
 	playing = false;
 	clearInterval(intervalId);
-  context.suspend();
+	context.suspend();
 }
 
 function play() {
